fdd59130ac18a35425e406ed13fe04b1c8091ccc
Brandon Andrews <bandrews568@gmail.com>
2017-04-13 19:28:24 -0500

    Generate button on random fragment now works

--- app/src/main/java/us/brandonandrews/nclottery/fragments/RandomFragment.java
+++ app/src/main/java/us/brandonandrews/nclottery/fragments/RandomFragment.java
@@ -20,6 +20,9 @@ import us.brandonandrews.nclottery.utils.GameData;
 
 public class RandomFragment extends Fragment {
 
+    private Spinner spinner;
+    private FrameLayout frameLayout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,68 +32,13 @@ public class RandomFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         Button btnGenerate = (Button) view.findViewById(R.id.btnGenerate);
-        final Spinner spinner = (Spinner) view.findViewById(R.id.randomSpinner);
-        final FrameLayout frameLayout = (FrameLayout) view.findViewById(R.id.randomFrameLayout);
+        spinner = (Spinner) view.findViewById(R.id.randomSpinner);
+        frameLayout = (FrameLayout) view.findViewById(R.id.randomFrameLayout);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                LayoutInflater layoutInflater = getLayoutInflater(null);
-                int layout;
-                String[] numbers;
-                String gameName = null;
-                frameLayout.removeAllViews();
-
-                switch(spinner.getSelectedItem().toString()) {
-                    case "Pick 3":
-                        layout = R.layout.pick3_listview;
-                        numbers = generateRandomNumbers(9, 3);
-                        View inflatedPick3 = layoutInflater.from(getContext())
-                                .inflate(layout, frameLayout, false);
-                        frameLayout.addView(GameData.setupPick3View(inflatedPick3, numbers));
-                        break;
-                    case "Pick 4":
-                        layout = R.layout.pick4_listview;
-                        numbers = generateRandomNumbers(9, 4);
-                        View inflatedPick4 = layoutInflater.from(getContext())
-                                .inflate(layout, frameLayout, false);
-                        frameLayout.addView(GameData.setupPick4View(inflatedPick4, numbers));
-                        break;
-                    case "Cash 5":
-                        layout = R.layout.cash5_listview;
-                        numbers = generateUniqueRandomNumbers(41, 5);
-                        View inflatedCash5 = layoutInflater.from(getContext())
-                                .inflate(layout, frameLayout, false);
-                        frameLayout.addView(GameData.setupCash5View(inflatedCash5, numbers));
-                        break;
-                    case "Lucky For Life":
-                        layout = R.layout.lucky_for_life_listview;
-                        numbers = generateUniqueRandomNumbers(48, 5);
-                        String luckyBall = generateSingleBall(18);
-                        View inflatedLuckyForLife = layoutInflater.from(getContext())
-                                .inflate(layout, frameLayout, false);
-                        frameLayout.addView(GameData
-                                .setupLuckyForLifeView(inflatedLuckyForLife, numbers, luckyBall));
-                        break;
-                    case "Mega Millions":
-                        layout = R.layout.mega_millions_listview;
-                        numbers = generateUniqueRandomNumbers(75, 5);
-                        String megaBall = generateSingleBall(15);
-                        View inflatedMegaMillions = layoutInflater.from(getContext())
-                                .inflate(layout, frameLayout, false);
-                        frameLayout.addView(GameData
-                                .setupMegaMillionsView(inflatedMegaMillions, numbers, megaBall));
-                        break;
-                    case "Powerball":
-                        layout = R.layout.powerball_listview;
-                        numbers = generateUniqueRandomNumbers(69, 5);
-                        String powerball = generateSingleBall(26);
-                        View inflatedPowerball = layoutInflater.from(getContext())
-                                .inflate(layout, frameLayout, false);
-                        frameLayout.addView(GameData
-                                .setupPowerballView(inflatedPowerball, numbers, powerball));
-                        break;
-                }
+                setupFrameLayout(spinner.getSelectedItem().toString());
             }
 
             @Override
@@ -102,9 +50,66 @@ public class RandomFragment extends Fragment {
         btnGenerate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO have this refresh the framelayout with new data
+                setupFrameLayout(spinner.getSelectedItem().toString());
             }
         });
+    }
+
+    private void setupFrameLayout(String game) {
+        int layout;
+        String[] numbers;
+        frameLayout.removeAllViews();
+
+        switch(game) {
+            case "Pick 3":
+                layout = R.layout.pick3_listview;
+                numbers = generateRandomNumbers(9, 3);
+                View inflatedPick3 = LayoutInflater.from(getContext())
+                        .inflate(layout, frameLayout, false);
+                frameLayout.addView(GameData.setupPick3View(inflatedPick3, numbers));
+                break;
+            case "Pick 4":
+                layout = R.layout.pick4_listview;
+                numbers = generateRandomNumbers(9, 4);
+                View inflatedPick4 = LayoutInflater.from(getContext())
+                        .inflate(layout, frameLayout, false);
+                frameLayout.addView(GameData.setupPick4View(inflatedPick4, numbers));
+                break;
+            case "Cash 5":
+                layout = R.layout.cash5_listview;
+                numbers = generateUniqueRandomNumbers(41, 5);
+                View inflatedCash5 = LayoutInflater.from(getContext())
+                        .inflate(layout, frameLayout, false);
+                frameLayout.addView(GameData.setupCash5View(inflatedCash5, numbers));
+                break;
+            case "Lucky For Life":
+                layout = R.layout.lucky_for_life_listview;
+                numbers = generateUniqueRandomNumbers(48, 5);
+                String luckyBall = generateSingleBall(18);
+                View inflatedLuckyForLife = LayoutInflater.from(getContext())
+                        .inflate(layout, frameLayout, false);
+                frameLayout.addView(GameData
+                        .setupLuckyForLifeView(inflatedLuckyForLife, numbers, luckyBall));
+                break;
+            case "Mega Millions":
+                layout = R.layout.mega_millions_listview;
+                numbers = generateUniqueRandomNumbers(75, 5);
+                String megaBall = generateSingleBall(15);
+                View inflatedMegaMillions = LayoutInflater.from(getContext())
+                        .inflate(layout, frameLayout, false);
+                frameLayout.addView(GameData
+                        .setupMegaMillionsView(inflatedMegaMillions, numbers, megaBall));
+                break;
+            case "Powerball":
+                layout = R.layout.powerball_listview;
+                numbers = generateUniqueRandomNumbers(69, 5);
+                String powerball = generateSingleBall(26);
+                View inflatedPowerball = LayoutInflater.from(getContext())
+                        .inflate(layout, frameLayout, false);
+                frameLayout.addView(GameData
+                        .setupPowerballView(inflatedPowerball, numbers, powerball));
+                break;
+        }
     }
 
     private String[] generateRandomNumbers(int max, int amount) {
